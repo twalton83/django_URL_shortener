ace40506a73d5863757941c62e01f8773d4f5b47
Tatiana <hello@tatianacodes.com>
2021-09-22 20:46:45 -0700

    Set up basic GET route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Form from "./components/Form";
 import Footer from "./components/Footer";
 
 function App() {
+  useEffect(() => {
+    fetch(process.env.REACT_APP_BACKEND_URL!).then((res) =>
+      res.json().then((data) => console.log(data))
+    );
+  });
   return (
     <div className="App">
       <Header />
